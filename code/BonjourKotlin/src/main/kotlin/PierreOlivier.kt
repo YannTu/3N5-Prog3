fac6483a7a490beb2ba54d47b39f008c793f22d1
6223399 <6223399@cegepmontpetit.ca>
2024-08-22 12:04:25 -0400

    Commit Exercice Bonjour Kotlin

---
+++ code/BonjourKotlin/src/main/kotlin/PierreOlivier.kt
@@ -0,0 +1,8 @@
+package org.tupayachi
+
+fun main() {
+    val name = "Pierre-Olivier"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Salut " + name)
+}
