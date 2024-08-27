e3f459095e194f0992219b8ab9b12296a3fabbe8
YannTu <6223399@cegepmontpetit.ca>
2024-08-27 15:30:54 -0400

    Exercice librairie JSOUP

---
+++ code/DemoJSoup/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
+fun main() {
+    val doc: Document = Jsoup.connect("https://en.wikipedia.org/").get()
+    println(doc.title())
+
+    val newsHeadlines: Elements = doc.select("#mp-itn b a")
+    for (headline: Element in newsHeadlines) {
+        println("${headline.attr("title")}\n\t${headline.absUrl("href")}")
+    }
+}
