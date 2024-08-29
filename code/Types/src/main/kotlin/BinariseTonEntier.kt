0b82871dcba717cd8a9fe4dbf64586d4221ce63a
6223399 <6223399@cegepmontpetit.ca>
2024-08-29 13:00:47 -0400

    Exercice BinariseTonEntier Progress

---
+++ code/Types/src/main/kotlin/BinariseTonEntier.kt
@@ -0,0 +1,14 @@
+package org.tupayachi
+
+fun main(args: Array<String>) {
+    val lenght = args[0].length - 1
+    if(args.isEmpty()) {
+        print("Aucune chaîne de charactères")
+    }
+    else {
+        for (i: Int in 0..lenght) {
+
+        }
+    }
+
+}
