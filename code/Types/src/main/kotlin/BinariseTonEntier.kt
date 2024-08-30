c3e9323028cda3b32f1f4a93a474756e2563d300
YannTu <6223399@cegepmontpetit.ca>
2024-08-30 01:33:49 -0400

    Exercice BinariseTonEntier

--- code/Types/src/main/kotlin/BinariseTonEntier.kt
+++ code/Types/src/main/kotlin/BinariseTonEntier.kt
@@ -1,14 +1,15 @@
 package org.tupayachi
 
 fun main(args: Array<String>) {
-    val lenght = args[0].length - 1
     if(args.isEmpty()) {
         print("Aucune chaîne de charactères")
     }
     else {
-        for (i: Int in 0..lenght) {
-
+        try {
+            val nombre = args[0].toInt()
+            print(nombre.toString(2))
+        } catch (e: Exception) {
+            println("Cet argument n'est pas un nombre")
         }
     }
-
 }
