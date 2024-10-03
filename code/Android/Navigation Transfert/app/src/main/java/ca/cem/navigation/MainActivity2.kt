59bf2c683325c96af09397ccb336f5c744f95b41
6223399 <6223399@cegepmontpetit.ca>
2024-10-03 12:58:01 -0400

    Progres Exercice Navigation Transfert

---
+++ code/Android/Navigation Transfert/app/src/main/java/ca/cem/navigation/MainActivity2.kt
@@ -0,0 +1,31 @@
+package ca.cem.navigation
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ca.cem.navigation.databinding.ActivityMain2Binding
+import ca.cem.navigation.databinding.ActivityMainBinding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        title = "Article"
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bouton1Clique : Button = binding.btnContact2 as Button
+        bouton1Clique.setOnClickListener {
+            startActivity(Intent(this, MainActivity3::class.java))
+        }
+    }
+
+}
---
+++ code/Android/Navigation Transfert/app/src/main/java/ca/cem/navigation/MainActivity3.kt
@@ -0,0 +1,29 @@
+package ca.cem.navigation
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ca.cem.navigation.databinding.ActivityMain3Binding
+import ca.cem.navigation.databinding.ActivityMainBinding
+
+class MainActivity3 : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMain3Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        title = "Contact"
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain3Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bouton1Clique : Button = binding.btnAccueil3 as Button
+        bouton1Clique.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
