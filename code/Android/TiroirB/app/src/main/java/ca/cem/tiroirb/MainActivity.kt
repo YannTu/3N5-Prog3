f4eba3d5aa975b6ad3f6fcdac0b5137c0a61b2f5
6223399 <6223399@cegepmontpetit.ca>
2024-11-14 12:24:18 -0500

    Exercice Tiroir

---
+++ code/Android/TiroirB/app/src/main/java/ca/cem/tiroirb/MainActivity.kt
@@ -0,0 +1,110 @@
+package ca.cem.tiroirb
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ca.cem.tiroirb.R
+import ca.cem.tiroirb.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupDrawer()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.mon_menu, menu)
+        return true
+    }
+
+    private fun setupDrawer() {
+        setupDrawerApplicationBar()
+        setupDrawerItemSelected()
+    }
+
+    private fun setupDrawerApplicationBar() {
+        // Afficher le menu hamburger sur la barre d'application
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        // Lier le tiroir de navigation à l'activité
+        // R.string.ouvert et R.string.ferme sont des strings de ressource.
+        // Référez-vous à la recette pour les strings de ressource pour voir comment les ajouter :
+        // https://info.cegepmontpetit.ca/3N5-Prog3/recettes/ressources-string
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, binding.dlTiroir, R.string.ouvert, R.string.ferme)
+
+        // Faire en sorte que le menu hamburger se transforme en flèche au clic, et vis versa
+        binding.dlTiroir.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+    }
+
+    private fun setupDrawerItemSelected() {
+        // Réagir aux clics sur les actions de menu
+        binding.nvTiroir.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.accueil_item -> {
+                    finish()
+                }
+                R.id.profil_item -> {
+                    Toast.makeText(this, "Yann", Toast.LENGTH_SHORT
+                    ).show()
+                }
+                R.id.deconnexion_item -> {
+                    Log.i("MainActivity", "Déconnexion réussie")
+                    Snackbar.make(
+                        binding.root, "Déconnexion réussie", Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            false
+        }
+    }
+
+
+    // Se déclenche lorsqu'un élément de la barre d'application est sélectionné
+    // Exemple : lorsqu'on clique sur le menu hamburger
+    // Peut aussi se combiner avec les autres options de menu qui se retrouvent dans la barre d'application
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Réagir au clic sur le menu hamburger
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // Les deux méthodes suivantes permettent de synchroniser le menu hamburger
+    // après avoir effectué une rotation de l'écran
+    // Pour mieux comprendre :
+    // 1. Commentez ces deux méthodes
+    // 2. Relancer l'application
+    // 3. Ouvrez le menu hamburger
+    // 4. Tournez le péripérique pour qu'il soit en mode paysage
+    // 5. Notez ce qui est arrivé au menu hambuger
+    // 6. Recommencez, mais en décommentant les deux méthodes
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        actionBarDrawerToggle.onConfigurationChanged(newConfig)
+        super.onConfigurationChanged(newConfig)
+    }
+}
